e79493de77f287896868102efba2287d202d09f5
Rishi Varma Kalidindi <rishivarmak.developer@gmail.com>
2024-06-14 21:40:24 -0400

    coded the basic ui of the pages

---
+++ firebase.js
@@ -0,0 +1,15 @@
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+  // Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDHjKQU8hbaewKt7lIGndX1U8FnXA75jbg",
+    authDomain: "cricscores-53a85.firebaseapp.com",
+    projectId: "cricscores-53a85",
+    storageBucket: "cricscores-53a85.appspot.com",
+    messagingSenderId: "988533352800",
+    appId: "1:988533352800:web:796d6aad53e9ae32389605"
+};
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ index.js
@@ -0,0 +1,12 @@
+
+function watch_clicked() {
+	window.location.replace("watcher.html")
+}
+
+function score_clicked() {
+	window.location.replace("scorer.html")
+}
+
+function body_loaded() {
+
+}
